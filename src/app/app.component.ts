375bee749de9ffd2d7bcdbb59b69ae4381909ca3
mathieu gaume <lechi@DESKTOP-QRL5LSB.(none)>
2017-05-16 13:49:09 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,50 @@
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { Contact } from '../pages/contact/contact';
+import { Importation } from '../pages/importation/importation';
+import { Parametres } from '../pages/parametres/parametres';
+
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
+  rootPage: any = Contact;
+
+  //Tableau permettant de lister toutes mes pages afin de lister dans le menu
+  pages: Array<{title: string, component: any}>;
+
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+    this.initializeApp();
+
+    //Utiliser par le ngFor dans le menu
+    this.pages = [
+      { title: 'Contact', component: Contact },
+      { title: 'Importation', component: Importation },
+      { title: 'Paramètres', component: Parametres }
+
+    ];
+
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    //this.nav.push(page.component);
+    this.nav.setRoot(page.component);
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule, NavController} from 'ionic-angular';
+
+import { MyApp } from './app.component';
+import { Contact } from '../pages/contact/contact';
+import { Importation } from '../pages/importation/importation';
+import { Parametres } from '../pages/parametres/parametres';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { Facebook } from '@ionic-native/facebook'
+
+@NgModule({
+  declarations: [
+    MyApp,
+    Contact,
+    Importation,
+    Parametres
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    Contact,
+    Importation,
+    Parametres
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    Facebook,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
